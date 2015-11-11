15afd29942b4054980f3e7865fed4851f29e4a57
Logan <Logjac@gmail.com>
2015-11-11 13:41:41 -0500

    Added skeleton framework for all functions
    Still need to create the content

--- lib/repos.rb
+++ lib/repos.rb
@@ -1,8 +1,42 @@
 # repos module
 
+# See link for details:
+# https://docs.google.com/document/d/1r3-NquhyRLbCncqTOQPwsznSZ-en6G6xzLbWIAmxhys/edit#heading=h.7pyingf1unu
+
 module Repos
   class Repos
     def initialize()
+    end
+    
+    def make_snapshot(file_array)
+    # Return hash ID of snapshot
+    return 1
+    end
+    
+    def restore_snapshot(target_id)
+    # Return comm object with status
+    end
+    
+    def history(branch_name)
+    # Return array of snapshot IDs
+    return ['a', 'b', 'c']
+    end
+    
+    def delete_snapshot(target_id)
+    # Return comm object with status
+    end
+    
+    def diff_snapshots(id1, id2)
+    # Return list of filenames and versions
+    end
+    
+    def make_branch(branch_name)
+    # Return hash ID of new branch
+    return 2
+    end
+    
+    def delete_branch(branch_name)
+    # Exit status code
     end
   end
 end
