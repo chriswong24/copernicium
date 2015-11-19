7602fb22397f4aea02a6a93643ed1215080f02ff
Frank Tamburrino <ftamburr@u.rochester.edu>
2015-11-19 03:46:25 -0500

    cleaned up tests and mocked up functionalisty that relies on other modules

--- test/PushPull/tpp.rb
+++
@@ -1,4 +0,0 @@
-require_relative 'PushPull'
-
-mod = Copernicium_PushPull::PushPull.new
-mod.connect("cycle2.csug.rochester.edu");
--- test/tc_pushpull.rb
+++ test/tc_pushpull.rb
@@ -6,53 +6,58 @@ require 'io/console'    # Needed to hide password at console
 require_relative 'test_helper.rb'
 
 class TestPushPullModule < Minitest::Test	
-  describe Copernicium_PushPull::PushPull do
+  describe PushPull::PushPull do
     before "initialize the PushPull Module" do
-      @inst = Copernicium_PushPull::PushPull.new
+      @inst = PushPull::PushPull.new
     end
-    
+
     it "is able to connect to a remote computer" do	# test for a good connection and a bad connection
-      conn = @inst.connect("cycle2.csug.rochester.edu")
+      conn = @inst.connect("cycle2.csug.rochester.edu", @user, @passwd)
       conn.must_equal true
 		
-      conn = @inst.connect("null@cif.rochester.edu")
+      conn = @inst.connect("null@cif.rochester.edu", @user, @passwd)
       conn.must_equal false
     end
 
     it "can yield a remote connection to a block" do 
       test = Object.new
-      conn = @inst.connect("cycle3.csug.rochester.edu") do |x|
+      conn = @inst.connect("cycle3.csug.rochester.edu", @user, @passwd) do |x|
         test = (x.exec!("echo Blocks Working!")).strip;
-        puts test
       end
       test.must_equal "Blocks Working!"
     end
 	
-    it "can move files to remote servers" do
+    it "can move files to remote servers for push" do
       tfile = File.new("comm_t.copernicium", 'w')
       tfile.close
-      test = @inst.transfer("cycle3.csug.rochester.edu", "./comm_t.copernicium", "/u/ftamburr/comm_t.copernicium")
+      test = @inst.transfer("cycle3.csug.rochester.edu", "./comm_t.copernicium", "/localdisk/comm_t.copernicium", @user, @passwd)
       File.delete("comm_t.copernicium")
-      @inst.connect("cycle3.csug.rochester.edu") do |x|
-        puts(x.exec!("ls ~/comm_t.copernicium").strip)
-        x.exec!("rm ~/comm_t.copernicium")
+      @inst.connect("cycle3.csug.rochester.edu", @user, @passwd) do |x|
+        x.exec!("ls /localdisk/comm_t.copernicium")
+        x.exec!("rm /localdisk/comm_t.copernicium")
       end
       test.must_equal true
     end
-    
-    it "can push changes to a remote server" do
-      result = @inst.push(nil)
-      result.must_equal "Successful push, branch #{nil} updated\n"
-    end
 	
-    it "can pull changes from a server" do
-      result = @inst.pull(nil)
-      result.must_equal "Successful pull, branch #{nil} updated\n"
+    it "can fetch files from a server for pull" do
+      @inst.connect("cycle3.csug.rochester.edu", @user, @passwd) do |x|
+        x.exec!("touch /localdisk/comm_t.copernicium")
+      end
+      result = @inst.fetch("cycle3.csug.rochester.edu", "/localdisk/comm_t.copernicium", "./", @user, @passwd)
+      File.delete("./comm_t.copernicium")
+      result.must_equal true
     end
-	
+
     it "can clone a repository from a server" do
-      result = @inst.clone(nil)
-      result.must_equal "Successful clone, local repository created!\n"
+      conn = @inst.connect("cycle3.csug.rochester.edu", @user, @passwd) do |x|
+        x.exec!("mkdir /localdisk/.t_copernicium")
+        x.exec!("touch /localdisk/.t_copernicium/comm_t.copernicium");
+      end
+      result = @inst.clone("cycle3.csug.rochester.edu", "/localdisk/.t_copernicium", @user, @passwd)
+      conn = @inst.connect("cycle3.csug.rochester.edu", @user, @passwd) do |x|
+        x.exec!("rm -r .t_copernicium")
+      end
+      result.must_equal true
     end
   end
 end
