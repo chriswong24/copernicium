cc7b10330f2cea50a669b3c54b03e32e5040a6d0
Logan <Logjac@gmail.com>
2015-11-20 15:37:46 -0500

    Basic code implemented
    branches, restore and diff still need to be implimented

--- lib/repos.rb
+++ lib/repos.rb
@@ -1,6 +1,7 @@
 # repos module
 
 require 'digest'
+require 'marshal'
 
 # Details from this link:
 #   https://docs.google.com/document/d/1r3-NquhyRLbCncqTOQPwsznSZ-en6G6xzLbWIAmxhys/edit#heading=h.7pyingf1unu
@@ -42,13 +43,26 @@ module Repos
   class Snapshot
     def initialize(in_array)
       @files = in_array
+      @id = ""
       # id = hash of in array?
     end
-    # contains array of file objects
-    # other info? - ID
+    
+    def set_id(in_id)
+      @id = in_id
+    end
+    
+    # drop get prefix?
+    def get_id()
+      @id
+    end
+    
+    def get_files()
+      @files
+    end
     # Initialize hash at startup
     # Possible? Or problem with self object?
   end
+  
   class Repos
     def initialize()
       # Create manifest
@@ -61,6 +75,8 @@ module Repos
     def make_snapshot(file_array)
       # Return hash ID of snapshot
       Snapshot new_snap(file_array)
+      # Set ID, consider breaking up that line
+      new_snap.set_id(Digest::SHA256.hexdigest(Marshal.dump(new_snap)))
       manifest.append(new_snap)
       # Update manifest file?
       return new_snap.hash
@@ -69,7 +85,9 @@ module Repos
     def get_snapshot(target_id)
       # Return snapshot (or just contents) given id
       # Find snapshot
+      manifest.index{ |x| x.get_id() == "target_id" }
       # Return it
+      #return ret_snap
     end
     
     # Not sure how I'm gonna do this one
@@ -81,19 +99,22 @@ module Repos
     def history(branch_name)
       # Return array of snapshot IDs
       names_list = []
-      manifest.each {|x| names_list.append(x.id)}
+      manifest.each {|x| names_list.append(x.get_id())}
       return names_list
     end
     
     def delete_snapshot(target_id)
       # Return comm object with status
       # Find snapshot, delete from manifest/memory
+      manifst.delete_if { |x| x.get_id() == target_id }
+      # catch error
+      # update manifest file?
     end
     
     def diff_snapshots(id1, id2)
-    # Return list of filenames and versions
-    # Find snapshot1 and snapshot2
-    # Use revlog diff on each set of files? Look at Diffy
+      # Return list of filenames and versions
+      # Find snapshot1 and snapshot2
+      # Use revlog diff on each set of files? Look at Diffy
     end
     
     def make_branch(branch_name)
