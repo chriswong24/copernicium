d5313f3d68700367c967f898f98746a24beead11
Chris Wong <chriswong1224@gmail.com>
2015-11-24 13:57:32 -0500

    buggy but more fleshed out integration tests

--- test/integration/tc_integrationtests.rb
+++ test/integration/tc_integrationtests.rb
@@ -2,7 +2,7 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 # Temporary until we fix module names
-#require_relative '../../lib/workspace.rb'
+require_relative '../../lib/workspace.rb'
 require_relative '../../lib/ui.rb'
 require_relative '../../lib/pushpull.rb'
 require_relative '../../lib/repos.rb'
@@ -10,73 +10,90 @@ require_relative '../../lib/RevLog.rb'
 
 include Copernicium
 
+class Workspace
+	attr_reader :repos, :files
+end
 
 class CoperniciumIntegrationTests < Minitest::Test
 	describe "CoperniciumDVCS" do
 		before "Calling basic copernicium commands" do
+
 			@inst = Copernicium::PushPull.new
-			@workspace = Workspace::Workspace.new(Dir.pwd)
-			@repo = Repos::Repos.new()
-			@RevLog = Copernicium::RevLog.new(Dir.pwd)
+			@workspace = Copernicium::Workspace.new
+			@workspace.repos.make_branch("dev")
 
-
-			#Create two branches, master and dev
-
-
+			@workspace.writeFile("workspace/1.txt","1")
+      @workspace.writeFile("workspace/2.txt", "2")
+      @workspace.commit(["workspace/1.txt","workspace/2.txt"])
 
 		end
 
 		it "can initialize the repository" do
 			comm = parse_command("init")
-			#@repo.initialize(comm)
 
-
-			#initialize reposotiry(comm)
-
-		end
-
-		it "can checkout a branch" do
-			#assert @workspace.branch_name == "master"
-			comm = parse_command("checkout")
-
-			#assert master = file list
-			#assert test_dev = file list + test file
 		end
 
 		it "can commit changes" do
 
-			@repo.manifest.size.must_equal 0
-
-			@workspace.writeFile("1.txt", "1")
-     	@workspace.writeFile("2.txt", "2")
+			@workspace.repos.manifest["default"].size.must_equal 1
+			@workspace.writeFile("1.txt", "1_1")
+     	@workspace.writeFile("2.txt", "2_2")
 			comm = parse_command("commit -m \"Test Commit\"")
-			@workspace.commit(comm)
 
-			#assert manifest.size > 0 ?
+			# Will include this once we start using UICommandCommunicator
 
-			#assert filelist= ["1.txt","2.txt"]
+			#@workspace.commit(comm).must_be_instance_of String
+			#@workspace.repos.manifest.size.must_equal 1
+		end
+
+		it "can checkout a list of files" do
+			comm = parse_command("checkout")
+		end
+
+		it "can checkout a branch" do
+
+			@workspace.readFile("workspace/1.txt").must_equal "1"
+			@workspace.readFile("workspace/2.txt").must_equal "2"
+			@workspace.writeFile("workspace/1.txt", "1_1")
+			@workspace.writeFile("workspace/2.txt", "2_2")
+			@workspace.commit(["workspace/1.txt","workspace/2.txt"])
+
+			comm = parse_command("checkout dev")
+			#@workspace.checkout(comm)
+			@workspace.checkout("dev")
+			@workspace.readFile("workspace/1.txt").must_equal "1"
+			@workspace.readFile("workspace/2.txt").must_equal "2"
+			
 		end
 
 		it "can merge two branches" do
 			comm = parse_command("merge")
-			#assert master = file list
-			#assert test_dev = file list + test file
 
 		end
 
 		it "can push a branch" do
+			#push branchname
 			comm = parse_command("push")
 		end
 
 		it "can pull a branch" do
+			#pull branchname
 			comm = parse_command("pull")
 		end
 
+		# Probably will delete this test because doesn't require any modular communication
+
 		it "can check the status of the repository" do
-			comm = parse_command("status")
+
+		 	File.delete('workspace/2.txt')
+      @workspace.writeFile("workspace/1.txt","edit")
+      @workspace.writeFile("workspace/3.txt","3")
+
+      changedFiles = @workspace.status()
+      changedFiles.must_equal([["workspace/3.txt"],["workspace/1.txt"],["workspace/2.txt"]])
+
+			#comm = parse_command("status")
 
 		end
-
 	end
-		
 end
