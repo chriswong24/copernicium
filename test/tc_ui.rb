1dfc72308d9cfa22424cf15aaa49aedd14ff4345
Jeremy Warner <jeremywrnr@gmail.com>
2015-11-25 17:41:40 -0500

    commit working

--- test/tc_ui.rb
+++ test/tc_ui.rb
@@ -9,12 +9,10 @@
 
 
 require_relative 'test_helper'
-
 include Copernicium
 
 
 class TestUI < Minitest::Test
-
   describe "UIModule" do
 
     def ui_test_helper(comm, cmd, files=nil, rev=nil, msg=nil, repo=nil)
@@ -28,7 +26,7 @@ class TestUI < Minitest::Test
 
     it "supports 'init' command" do
       comm = parse_command ["init"]
-      ui_test_helper(comm, 'init')
+      ui_test_helper(comm, "init")
     end
 
     it "supports 'status' command" do
@@ -36,23 +34,22 @@ class TestUI < Minitest::Test
       ui_test_helper(comm, "status")
     end
 
-    #it "supports 'commit' command" do
-      ## -m is optional, but if the user doesn't give it, the UI will prompt for a message in an
-      ## editor. Thus, the UICommandCommunicator will always include a commit message.
-      #comm = parse_command %w{commit -m a commit message}
-      #ui_test_helper(comm, "commit", nil, nil, "a commit message")
+    # -m is optional, but if the user doesn't give it, the UI will prompt for
+    # a message in an editor. Thus, the UICommandCommunicator will always
+    # include a commit message.
+    it "supports 'commit' command" do
+      comm = parse_command %w{commit -m a commit message}
+      ui_test_helper(comm, "commit", nil, nil, "a commit message")
 
-      #comm = parse_command "commit -m a commit message"
-      #ui_test_helper(comm, "commit", nil, nil, "a commit message")
+      comm = parse_command %w{commit -m a \strange commit $message}
+      ui_test_helper(comm, "commit", nil, nil, 'a \strange commit $message')
+    end
 
-      #comm = parse_command 'commit -m "a commit message"'
-      #ui_test_helper(comm, "commit", nil, nil, "a commit message")
-    #end
-
+    # Two valid forms of "checkout" command:
+    # cn checkout revision (checks out full repo at revision)
+    # cn checkout revision file.txt (checks out only the specified files
+    # from revision)
     #it "supports 'checkout' command" do
-      ## Two valid forms of "checkout" command:
-      ##   cn checkout revision              (checks out full repo at revision)
-      ##   cn checkout revision file.txt     (checks out only the specified files from revision)
       #comm = parse_command "checkout revID"
       #ui_test_helper(comm, "checkout", nil, "revID")
 
@@ -64,40 +61,38 @@ class TestUI < Minitest::Test
     #end
 
     #it "supports 'pull' command" do
-      #comm = parse_command "pull"
-      #comm.must_be_instance_of UICommandCommunicator
-      #comm.command.must_equal "pull"
-      #comm.files.must_be_nil
-      #comm.rev.must_be_nil
-      #comm.commit_message.must_be_nil
+    #comm = parse_command "pull"
+    #comm.must_be_instance_of UICommandCommunicator
+    #comm.command.must_equal "pull"
+    #comm.files.must_be_nil
+    #comm.rev.must_be_nil
+    #comm.commit_message.must_be_nil
     #end
 
     #it "supports 'push' command" do
-      #comm = parse_command "push"
-      #comm.must_be_instance_of UICommandCommunicator
-      #comm.command.must_equal "push"
-      #comm.files.must_be_nil
-      #comm.rev.must_be_nil
-      #comm.commit_message.must_be_nil
+    #comm = parse_command "push"
+    #comm.must_be_instance_of UICommandCommunicator
+    #comm.command.must_equal "push"
+    #comm.files.must_be_nil
+    #comm.rev.must_be_nil
+    #comm.commit_message.must_be_nil
     #end
 
     #it "supports 'merge' command" do
-      #comm = parse_command "merge some_revision" # Merge some_revision into current branch
-      #comm.must_be_instance_of UICommandCommunicator
-      #comm.command.must_equal "merge"
-      #comm.files.must_be_nil
-      #comm.rev.must_equal "some_revision"
-      #comm.commit_message.must_be_nil
+    #comm = parse_command "merge some_revision" # Merge some_revision into current branch
+    #comm.must_be_instance_of UICommandCommunicator
+    #comm.command.must_equal "merge"
+    #comm.files.must_be_nil
+    #comm.rev.must_equal "some_revision"
+    #comm.commit_message.must_be_nil
     #end
 
     #it "supports 'clone' command" do
-      ## Format:
-      ##   cn clone path-to-remote-repository
-      #comm = parse_command "clone ssh://user@some-host.com/some/repo/path"
-      #ui_test_helper(comm, "clone", nil, nil, nil, "ssh://user@some-host.com/some/repo/path")
+    ## Format:
+    ##   cn clone path-to-remote-repository
+    #comm = parse_command "clone ssh://user@some-host.com/some/repo/path"
+    #ui_test_helper(comm, "clone", nil, nil, nil, "ssh://user@some-host.com/some/repo/path")
     #end
->>>>>>> better handling of args
-
   end
 end
 
