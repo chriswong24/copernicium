07558c014d82d573676ac9fffaadc503bd03710d
Jeremy Warner <jeremywrnr@gmail.com>
2015-11-25 17:32:45 -0500

    better handling of args

--- lib/ui.rb
+++ lib/ui.rb
@@ -7,66 +7,55 @@ module Copernicium
   #   * argv - an array containing the tokenized command line from the user
   #       For instance:
   #         "cn hello world" -> ['hello', 'world']
+  #   * cmds - the text command line given by the user
+  #   * actually should be an array of arguments, eg:
+  #   * cn hello world -> ['hello', 'world']
   #
   # Return value:
   #   A UICommandCommunicator object containing details of the command to be
   #   executed by the respective backend module.
   #
-  def parse_command(argv)
-    #
-    # Convert the array of arguments to a space-separated string (i.e., as it
-    # would actually be entered on the command line). This makes it easier to
-    # parse some commands, such as commit messages given with "commit -m".
-    #
-    # (Some of the commands could probably be more easily parsed by dealing
-    # directly, with argv, i.e., I'm just splitting them back up, but I've
-    # already written the code and it's most important to get it working. This
-    # can be refactored later if we have time, but in the meantime, it should
-    # be sound, if slightly roundabout.)
-    #
-    cmd = ""
-    argv.each do |arg|
-      cmd << arg
-      cmd << " "
-    end
-    cmd.chop! # Remove extra space at the end
-    
-    # Handle no-argument commands
-    if cmd == "init" or cmd == "status" or cmd == "push" or cmd == "pull"
-      return UICommandCommunicator.new(command: cmd)
+  def parse_command(cmds)
+
+    # handle no arguments given - show help information
+    # TODO
+
+    %w{init status push pull}.each do |noarg| # Handle no-argument commands
+      if cmds.first == noarg
+        first = cmds.shift
+        return UICommandCommunicator.new(command: first, opts: cmds)
+      end
     end
 
     # Handle "commit"
-    if cmd.start_with? "commit"
-      cmd_message_split = cmd.partition(" -m ")
-
-      if cmd_message_split.count != 3 or cmd_message_split[2] == ""
-        print "Error: no commit message (or multiple messages) given for command 'commit'!\n"
-        return nil
-      end
-
-      commit_msg = cmd_message_split[2]
-
-      if commit_msg.length == 0
+    if cmds.first == "commit"
+      messflag = cmds.find_index("-m")
+      if (messflag.nil?)
         print "Error: commit message is empty"
         # TODO: launch editor and get long commit message
+        # alternatively just prompt them for a commit message
+        # using rubys 'gets', and use that as the message
+        # refactor this into single method, use below
         return nil
       end
 
-      # Filter quotes around the commit message, if present
-      # (If the quotes around the commit message do not match, we will not filter them, i.e.,
-      # they'll be considered an intentional part of the message.)
-      if (commit_msg[0] == '"' and commit_msg[commit_msg.length - 1] == '"') \
-        or (commit_msg[0] == "'" and commit_msg[commit_msg.length - 1] == "'")
+      # mash everything after the -m flag into a single string
+      message = cmds[messflag+1..-1].join ' '
 
-        commit_msg = commit_msg.slice(1, commit_msg.length - 2)
+      if message == '' || message.nil?
+        print "Error: commit message is empty"
+        # TODO: launch editor and get long commit message
+        # alternatively just prompt them for a commit message
+        # using rubys 'gets', and use that as the message
+        return nil
       end
 
-      return UICommandCommunicator.new(command: "commit", commit_message: commit_msg)
+      return UICommandCommunicator.new(command: "commit",
+                                       commit_message: message)
     end
 
     # Handle "merge"
-    if cmd.start_with? "merge"
+    if cmds.start_with? "merge"
       cmd_split = cmd.split(" ")
 
       if cmd_split.count != 2
@@ -78,7 +67,7 @@ module Copernicium
     end
 
     # Handle "checkout"
-    if cmd.start_with? "checkout"
+    if cmds.start_with? "checkout"
       cmd_split = cmd.split(" ")
 
       if cmd_split.count < 2
@@ -96,7 +85,7 @@ module Copernicium
     end
 
     # Handle "clone"
-    if cmd.start_with? "clone"
+    if cmds.start_with? "clone"
       cmd_split = cmd.split(" ")
 
       if cmd_split.count != 2
@@ -110,21 +99,20 @@ module Copernicium
 
   # Communication object that will pass commands to backend modules
   class UICommandCommunicator
-
-    attr_reader :command
-
-    # Types of arguments - different fields will be set depending on the command
-    attr_accessor :files # An array of one or more file paths
-    attr_accessor :rev # A single revision indicator (commit #, branch name, HEAD, tip, etc.)
-    attr_accessor :commit_message # A commit message
-    attr_accessor :repo # URL/path to a remote repository
-
-    def initialize(command: nil, files: nil, rev: nil, commit_message: nil, repo: nil)
+    attr_reader :command, :arguments, :files, :rev, :commit_message, :repo
+    # Types of arguments:
+    # files    -  An array of one or more file paths
+    # rev      - A single revision indicator (commit #, branch name, HEAD, etc.)
+    # repo     - URL/path to a remote repository
+    # commit_message - A commit message
+    def initialize(command: nil, files: nil, rev: nil,
+                   commit_message: nil, repo: nil, opts: nil)
+      @commit_message = commit_message
       @command = command
       @files = files
-      @rev = rev
-      @commit_message = commit_message
+      @opts = opts
       @repo = repo
+      @rev = rev
     end
   end
 end
--- test/tc_ui.rb
+++ test/tc_ui.rb
@@ -36,66 +36,67 @@ class TestUI < Minitest::Test
       ui_test_helper(comm, "status")
     end
 
-    it "supports 'commit' command" do
-      # -m is optional, but if the user doesn't give it, the UI will prompt for a message in an
-      # editor. Thus, the UICommandCommunicator will always include a commit message.
-      comm = parse_command "commit -m 'a commit message'".split(" ")
-      ui_test_helper(comm, "commit", nil, nil, "a commit message")
+    #it "supports 'commit' command" do
+      ## -m is optional, but if the user doesn't give it, the UI will prompt for a message in an
+      ## editor. Thus, the UICommandCommunicator will always include a commit message.
+      #comm = parse_command %w{commit -m a commit message}
+      #ui_test_helper(comm, "commit", nil, nil, "a commit message")
 
-      comm = parse_command "commit -m a commit message".split(" ")
-      ui_test_helper(comm, "commit", nil, nil, "a commit message")
+      #comm = parse_command "commit -m a commit message"
+      #ui_test_helper(comm, "commit", nil, nil, "a commit message")
 
-      comm = parse_command 'commit -m "a commit message"'.split(" ")
-      ui_test_helper(comm, "commit", nil, nil, "a commit message")
-    end
+      #comm = parse_command 'commit -m "a commit message"'
+      #ui_test_helper(comm, "commit", nil, nil, "a commit message")
+    #end
 
-    it "supports 'checkout' command" do
-      # Two valid forms of "checkout" command:
-      #   cn checkout revision              (checks out full repo at revision)
-      #   cn checkout revision file.txt     (checks out only the specified files from revision)
-      comm = parse_command "checkout revID".split(" ")
-      ui_test_helper(comm, "checkout", nil, "revID")
+    #it "supports 'checkout' command" do
+      ## Two valid forms of "checkout" command:
+      ##   cn checkout revision              (checks out full repo at revision)
+      ##   cn checkout revision file.txt     (checks out only the specified files from revision)
+      #comm = parse_command "checkout revID"
+      #ui_test_helper(comm, "checkout", nil, "revID")
 
-      comm = parse_command "checkout revID file.txt".split(" ")
-      ui_test_helper(comm, "checkout", ["file.txt"], "revID")
+      #comm = parse_command "checkout revID file.txt"
+      #ui_test_helper(comm, "checkout", ["file.txt"], "revID")
 
-      comm = parse_command "checkout revID file.txt foo.c".split(" ")
-      ui_test_helper(comm, "checkout", ["file.txt", "foo.c"], "revID")
-    end
+      #comm = parse_command "checkout revID file.txt foo.c"
+      #ui_test_helper(comm, "checkout", ["file.txt", "foo.c"], "revID")
+    #end
 
-    it "supports 'pull' command" do
-      comm = parse_command ["pull"]
-      comm.must_be_instance_of UICommandCommunicator
-      comm.command.must_equal "pull"
-      comm.files.must_be_nil
-      comm.rev.must_be_nil
-      comm.commit_message.must_be_nil
-    end
+    #it "supports 'pull' command" do
+      #comm = parse_command "pull"
+      #comm.must_be_instance_of UICommandCommunicator
+      #comm.command.must_equal "pull"
+      #comm.files.must_be_nil
+      #comm.rev.must_be_nil
+      #comm.commit_message.must_be_nil
+    #end
 
-    it "supports 'push' command" do
-      comm = parse_command ["push"]
-      comm.must_be_instance_of UICommandCommunicator
-      comm.command.must_equal "push"
-      comm.files.must_be_nil
-      comm.rev.must_be_nil
-      comm.commit_message.must_be_nil
-    end
+    #it "supports 'push' command" do
+      #comm = parse_command "push"
+      #comm.must_be_instance_of UICommandCommunicator
+      #comm.command.must_equal "push"
+      #comm.files.must_be_nil
+      #comm.rev.must_be_nil
+      #comm.commit_message.must_be_nil
+    #end
 
-    it "supports 'merge' command" do
-      comm = parse_command "merge some_revision".split(" ") # Merge some_revision into current branch
-      comm.must_be_instance_of UICommandCommunicator
-      comm.command.must_equal "merge"
-      comm.files.must_be_nil
-      comm.rev.must_equal "some_revision"
-      comm.commit_message.must_be_nil
-    end
+    #it "supports 'merge' command" do
+      #comm = parse_command "merge some_revision" # Merge some_revision into current branch
+      #comm.must_be_instance_of UICommandCommunicator
+      #comm.command.must_equal "merge"
+      #comm.files.must_be_nil
+      #comm.rev.must_equal "some_revision"
+      #comm.commit_message.must_be_nil
+    #end
 
-    it "supports 'clone' command" do
-      # Format:
-      #   cn clone path-to-remote-repository
-      comm = parse_command "clone ssh://user@some-host.com/some/repo/path".split(" ")
-      ui_test_helper(comm, "clone", nil, nil, nil, "ssh://user@some-host.com/some/repo/path")
-    end
+    #it "supports 'clone' command" do
+      ## Format:
+      ##   cn clone path-to-remote-repository
+      #comm = parse_command "clone ssh://user@some-host.com/some/repo/path"
+      #ui_test_helper(comm, "clone", nil, nil, nil, "ssh://user@some-host.com/some/repo/path")
+    #end
+>>>>>>> better handling of args
 
   end
 end
