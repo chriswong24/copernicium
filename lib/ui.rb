b16a77163e92ee415196f36b8aa2c8b40c885693
Jeremy Warner <jeremywrnr@gmail.com>
2015-12-02 12:20:50 -0500

    adding support for status

--- lib/ui.rb
+++ lib/ui.rb
@@ -85,7 +85,6 @@ module Copernicium
     def status(args)
       ui = UIComm.new(command: 'status', opts: args)
       st = Workspace.new.status(ui)
-      puts st
       puts "added:".grn + st[0].join(', ')   unless st[0].empty?
       puts "edited:".yel + st[1].join(', ')   unless st[1].empty?
       puts "removed:".red + st[2].join(', ') unless st[2].empty?
