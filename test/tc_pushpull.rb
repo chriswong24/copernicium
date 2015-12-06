5b15adb3115579921a10e6bd06fc3ddd38e36b6a
Frank Tamburrino <ftamburr@u.rochester.edu>
2015-12-05 21:47:14 -0500

    updated tc_pushpull to reflect the new design of PushPull as a module

--- test/tc_pushpull.rb
+++ test/tc_pushpull.rb
@@ -7,21 +7,17 @@ require_relative 'test_helper'
 
 class TestPushPullModule < Minitest::Test
   describe Copernicium::PushPull do
-    before "initialize the PushPull Module" do
-      @inst = Copernicium::PushPull.new
-    end
-
     it "is able to connect to a remote computer" do	# test for a good connection and a bad connection
-      conn = @inst.connect("cycle2.csug.rochester.edu", @user)
+      conn = PushPull::connect("cycle2.csug.rochester.edu", @user)
       conn.must_equal true
 
-      conn = @inst.connect("null@cif.rochester.edu", @user)
+      conn = PushPull::connect("null@cif.rochester.edu", @user)
       conn.must_equal false
     end
 
     it "can yield a remote connection to a block" do
       test = Object.new
-      conn = @inst.connect("cycle2.csug.rochester.edu", @user) do |x|
+      conn = PushPull::connect("cycle2.csug.rochester.edu", @user) do |x|
         test = (x.exec!("echo Blocks Working!")).strip;
       end
       test.must_equal "Blocks Working!"
@@ -30,11 +26,11 @@ class TestPushPullModule < Minitest::Test
     it "can move files to remote servers for push" do
       tfile = File.new("comm_t.copernicium", 'w')
       tfile.close
-      test = @inst.transfer("cycle2.csug.rochester.edu", "./comm_t.copernicium", "/localdisk/comm_t.copernicium", @user) do |scp|
+      test = PushPull::transfer("cycle2.csug.rochester.edu", "./comm_t.copernicium", "/localdisk/comm_t.copernicium", @user) do |scp|
         scp.upload!("./comm_t.copernicium", "/localdisk/comm_t.copernicium", :recursive => true)
       end
       File.delete("comm_t.copernicium")
-      @inst.connect("cycle2.csug.rochester.edu", @user) do |x|
+      PushPull::connect("cycle2.csug.rochester.edu", @user) do |x|
         x.exec!("ls /localdisk/comm_t.copernicium")
         x.exec!("rm /localdisk/comm_t.copernicium")
       end
@@ -42,21 +38,21 @@ class TestPushPullModule < Minitest::Test
     end
 
     it "can fetch files from a server for pull" do
-      @inst.connect("cycle2.csug.rochester.edu", @user) do |x|
+      PushPull::connect("cycle2.csug.rochester.edu", @user) do |x|
         x.exec!("touch /localdisk/comm_t.copernicium")
       end
-      result = @inst.fetch("cycle2.csug.rochester.edu", "/localdisk/comm_t.copernicium", "./", @user)
+      result = PushPull::fetch("cycle2.csug.rochester.edu", "/localdisk/comm_t.copernicium", "./", @user)
       File.delete("./comm_t.copernicium")
       result.must_equal true
     end
 
     it "can clone a repository from a server" do
-      conn = @inst.connect("cycle2.csug.rochester.edu", @user) do |x|
+      conn = PushPull::connect("cycle2.csug.rochester.edu", @user) do |x|
         x.exec!("mkdir /localdisk/.t_copernicium")
         x.exec!("touch /localdisk/.t_copernicium/comm_t.copernicium");
       end
-      result = @inst.clone("cycle2.csug.rochester.edu:/localdisk/.t_copernicium", @user)
-      conn = @inst.connect("cycle2.csug.rochester.edu", @user) do |x|
+      result = PushPull::clone("cycle2.csug.rochester.edu:/localdisk/.t_copernicium", @user)
+      conn = PushPull::connect("cycle2.csug.rochester.edu", @user) do |x|
         x.exec!("rm -r .t_copernicium")
       end
       result.must_equal true
